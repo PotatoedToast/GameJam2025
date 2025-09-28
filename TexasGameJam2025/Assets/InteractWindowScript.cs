ec4064a75de5191aa7df8007f033a367286468aa
PotatoedToast <toby.huang.757@gmail.com>
2025-09-28 02:23:03 -0500

    toggling visibility enabled

--- TexasGameJam2025/Assets/InteractWindowScript.cs
+++ TexasGameJam2025/Assets/InteractWindowScript.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class InteractWindowScript : MonoBehaviour
 {
+     
+    [SerializeField] private GameObject _targetWindow;
+    private const float TOGGLE_INTERVAL = 5.0f; 
+    private float nextToggleTime = 0f; 
+
+
+
+     public void OpenWindow(){
+        if(_targetWindow != null){
+            _targetWindow.SetActive(true);
+        }
+     }
+
+     public void HideWindow(){
+        if(_targetWindow != null){
+            _targetWindow.SetActive(false);
+        }
+     }
+
+     public void ToggleWindow(){
+        if(_targetWindow != null){
+            _targetWindow.SetActive(!_targetWindow.activeSelf);
+        }
+     }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
+        HideWindow();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
